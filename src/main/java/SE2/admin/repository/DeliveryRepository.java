0188669641ba6c915deef074ad15895c41ea6546
thuynguyenbm <thuynguyenbm123@gmail.com>
2022-04-17 00:55:09 +0700

    merge fixed products, tested

--- src/main/java/SE2/admin/repository/DeliveryRepository.java
+++ src/main/java/SE2/admin/repository/DeliveryRepository.java
@@ -1,6 +1,6 @@
 package SE2.admin.repository;
 
-import SE2.Admin.model.DeliveryStatus;
+import SE2.admin.model.DeliveryStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
