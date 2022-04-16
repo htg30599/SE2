7d2671856570747ca6c461df363bee9f773b48da
htg30599 <1701040146@s.hanu.edu.vn>
2022-04-16 12:13:46 +0700

    commit

--- src/main/java/SE2/Admin/controller/DeliveryController.java
+++ src/main/java/SE2/Admin/controller/DeliveryController.java
@@ -1,8 +1,8 @@
 package SE2.admin.controller;
 
 
-import SE2.Admin.model.DeliveryStatus;
-import SE2.Admin.model.Status;
+import SE2.admin.model.DeliveryStatus;
+import SE2.admin.model.Status;
 import SE2.admin.repository.DeliveryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
