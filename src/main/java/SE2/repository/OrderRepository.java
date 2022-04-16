cc1d7ada4a7b72d4b87380246ff4368fc8c9ba41
MaiAnh <lemaianh23062001@gmail.com>
2022-04-16 22:45:03 +0700

    order

--- src/main/java/SE2/repository/OrderRepository.java
+++ src/main/java/SE2/repository/OrderRepository.java
@@ -2,9 +2,10 @@ package SE2.repository;
 
 import SE2.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long>{
 
     List<Order> findByNameContaining(String name);
