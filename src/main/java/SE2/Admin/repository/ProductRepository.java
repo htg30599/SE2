a07c1591be76341ea2acf0e6077dc83298c4e7f0
htg30599 <1701040146@s.hanu.edu.vn>
2022-04-16 10:52:02 +0700

    commit

--- src/main/java/SE2/Admin/repository/ProductRepository.java
+++
@@ -1,6 +0,0 @@
-package SE2.Admin.repository;
-
-import SE2.Admin.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface ProductRepository extends JpaRepository<Product, Long>{
-}
