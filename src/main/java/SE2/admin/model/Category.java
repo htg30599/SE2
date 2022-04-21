940bdc3fd1dbf4446e3a298ecf340b75001e5972
htg30599 <1701040146@s.hanu.edu.vn>
2022-04-22 00:34:28 +0700

    add

--- src/main/java/SE2/admin/model/Category.java
+++ src/main/java/SE2/admin/model/Category.java
@@ -8,7 +8,9 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
+
     private Long id;
+    @Column(name="name")
     private String name;
 
     public void setId(Long id) {
