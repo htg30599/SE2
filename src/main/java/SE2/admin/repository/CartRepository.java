8d76b66d947b73486e50aab9d1201724f7ce4133
thuynguyenbm <thuynguyenbm123@gmail.com>
2022-04-26 20:50:08 +0700

    update product

---
+++ src/main/java/SE2/admin/repository/CartRepository.java
@@ -0,0 +1,12 @@
+package SE2.admin.repository;
+
+import SE2.admin.model.Cart;
+import SE2.admin.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface CartRepository  extends JpaRepository<Category, Long> {
+    Cart findByUserEmail(String email);
+}
